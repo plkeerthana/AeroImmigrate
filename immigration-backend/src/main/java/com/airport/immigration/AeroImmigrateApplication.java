f09e0eb9578599b51c4c53f1f807908f548d1a8f
keerthansamudrala <kalashakaavtomat@gmail.com>
2025-08-16 16:42:07 +0530

    Fixed backend folder issue - added as normal files

---
+++ immigration-backend/src/main/java/com/airport/immigration/AeroImmigrateApplication.java
@@ -0,0 +1,14 @@
+package com.airport.immigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroImmigrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroImmigrateApplication.class, args);
+		
+	}
+
+}
---
+++ immigration-backend/src/test/java/com/airport/immigration/AeroImmigrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airport.immigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroImmigrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
